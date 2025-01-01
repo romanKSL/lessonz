f5eade902832be0281bb1835a606520f483a077c
romanKSL <romankyslyi42@gmail.com>
2025-01-01 11:00:33 +0200

    Added the Http client

--- src/main/java/com/kyslyi/lessonz/Application.java
+++ src/main/java/com/kyslyi/lessonz/Application.java
@@ -2,11 +2,15 @@ package com.kyslyi.lessonz;
 
 import com.kyslyi.lessonz.lesson.LessonRepository;
 import com.kyslyi.lessonz.user.User;
+import com.kyslyi.lessonz.user.UserHttpClient;
 import com.kyslyi.lessonz.user.UserRestClient;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 import java.util.List;
 
@@ -18,7 +22,14 @@ public class Application {
 	}
 
 	@Bean
-	CommandLineRunner runner(UserRestClient client) {
+	UserHttpClient userHttpClient() {
+		RestClient restClient = RestClient.create("https://jsonplaceholder.typicode.com/");
+		HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(RestClientAdapter.create(restClient)).build();
+		return factory.createClient(UserHttpClient.class);
+	}
+
+	@Bean
+	CommandLineRunner runner(UserHttpClient client) {
 		return args -> {
 			List<User> users = client.findAll();
 			System.out.println("Users: " + users);
